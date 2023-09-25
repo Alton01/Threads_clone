7da96a26de44af53120a93234d1b35fbfb42ece0
Altono1 <altonobinna11@gmail.com>
2023-09-25 16:08:20 +0100

    v4

--- Desktop/threads/components/forms/PostThread.tsx
+++ Desktop/threads/components/forms/PostThread.tsx
@@ -14,12 +14,12 @@ import {
 import { Textarea } from "@/components/ui/textarea";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
+import { useOrganization } from "@clerk/nextjs";
 
 import { usePathname, useRouter } from "next/navigation";
 
 import { ThreadValidation } from "@/lib/validations/thread";
 import { createThread } from "@/lib/actions/thread.actions";
-import { getRandomValues } from "crypto";
 
 // import { updateUser } from "@/lib/actions/user.actions";
 
@@ -38,6 +38,7 @@ interface Props {
 function PostThread({ userId }: { userId: string }) {
   const router = useRouter();
   const pathname = usePathname();
+  const { organization } = useOrganization();
 
   const form = useForm({
     resolver: zodResolver(ThreadValidation),
@@ -53,7 +54,7 @@ function PostThread({ userId }: { userId: string }) {
     await createThread({
       text: values.thread,
       author: userId,
-      communityId: null,
+      communityId: organization ? organization.id : null,
       path: pathname,
     });
     router.push("/");
